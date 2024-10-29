003a9676536936eb9ac654a3b0207ffd7348e5dd
NishthaTripathi <nishtha.tripathi@wisetechglobal.com>
2024-10-29 18:50:47 +0530

    Cors changes

--- src/main/java/com/wisetech/employee_management/api/DepartmentController.java
+++ src/main/java/com/wisetech/employee_management/api/DepartmentController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/department")
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class DepartmentController {
 
     private final DepartmentService departmentService;
